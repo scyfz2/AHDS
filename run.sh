29727084205541d89a1264cf0d62415a412b981f
fan <94053722+scyfz2@users.noreply.github.com>
2024-11-26 12:06:14 +0000

    Add files via upload

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@
 set -e
 
 # SLURM Job Submission Directives
-#SBATCH --job-name=AHDS_2553055      # Name of the job
-#SBATCH --partition=teach_cpu                 # Partition name
+#SBATCH --job-name=AHDS_2553055          # Name of the job
+#SBATCH --partition=teach_cpu            # Partition name
 #SBATCH --nodes=1                        # Number of nodes
 #SBATCH --ntasks=1                       # Number of tasks (typically the same as nodes for simple jobs)
 #SBATCH --cpus-per-task=1                # Number of cores per task
 #SBATCH --mem=16G                        # Memory per node
 #SBATCH --time=12:00:00                  # Time limit hrs:min:sec
-#SBATCH --output=AHDS_2553055_%j.log # Standard output and error log
+#SBATCH --output=AHDS_2553055_%j.log     # Standard output and error log
 #SBATCH --account=SSCM033324
 
 echo "Job started on $(date)"  # Using $(...) for command substitution
